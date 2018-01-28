8ec1fda243d07dbe4a47e26bd4026d3c047e2b4f
Caleb Hearon <caleb@chearon.net>
2018-01-28 13:00:07 -0500

    finish source map support

--- loader.js
+++ loader.js
@@ -1,6 +1,21 @@
-const transform = require("./transform");
+const transform = require('./transform');
+let SourceMapConsumer, SourceMapGenerator;
+
+try {
+  const sourceMap = require('source-map');
+  SourceMapConsumer = sourceMap.SourceMapConsumer;
+  SourceMapGenerator = sourceMap.SourceMapGenerator;
+} catch (e) {}
 
 module.exports = function (content) {
   const {code, map} = transform(content, this.resourcePath);
-  this.callback(null, code, map);
+  let finalMap = null;
+
+  if (SourceMapConsumer && SourceMapGenerator) {
+    const consumer = SourceMapConsumer(map);
+    finalMap = SourceMapGenerator.fromSourceMap(consumer);
+    finalMap.setSourceContent(this.resourcePath, content);
+  }
+
+  this.callback(null, code, finalMap && finalMap.toJSON());
 };
